6e91d8102d24928d292e526c767aed472848a722
bble <bblewong@hotmail.com>
2025-03-09 17:06:34 +0800

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  env: {
+    PINATA_API_KEY: process.env.PINATA_API_KEY,
+    PINATA_SECRET_API_KEY: process.env.PINATA_SECRET_API_KEY,
+  }
 };
 
 module.exports = nextConfig;
