1a66837ee70807d309eb3fb1519ffc644fdf31b5
bble <bblewong@hotmail.com>
2025-03-09 16:06:28 +0800

    Update ipfs.js

--- frontend/src/utils/ipfs.js
+++ frontend/src/utils/ipfs.js
@@ -1,6 +1,12 @@
 import { create } from "ipfs-http-client";
 
-const client = create("https://ipfs.infura.io:5001/api/v0");
+
+const client = create({
+  url: "https://ipfs.infura.io:5001/api/v0",
+  headers: {
+    authorization: "5a9ebf3fdfe04611b50e9fd157be0a00" 
+  }
+});
 
 export async function uploadFileToIPFS(file) {
     try {
