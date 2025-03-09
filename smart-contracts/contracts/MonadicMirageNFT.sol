ec2daec0d7ad7f75eea6aa4fbc2bb4fcf1f73373
bble <bblewong@hotmail.com>
2025-03-09 20:50:10 +0800

    Update MonadicMirageNFT.sol

--- smart-contracts/contracts/MonadicMirageNFT.sol
+++ smart-contracts/contracts/MonadicMirageNFT.sol
@@ -7,15 +7,26 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract MonadicMirageNFT is ERC721URIStorage, Ownable {
     uint256 private nextTokenId;
 
-    constructor() ERC721("MonadicMirageNFT", "MMNFT") {}
+    constructor() ERC721("Monadic Mirage NFT", "MMNFT") Ownable(msg.sender) {
+        nextTokenId = 1; // 初始化 tokenId
+    }
 
-    function mintNFT(string memory tokenURI) public {
+    function mintNFT(string memory metadataURI) public {
         require(balanceOf(msg.sender) == 0, "You already own an NFT");
 
         uint256 tokenId = nextTokenId;
         _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, tokenURI);  // 存储 IPFS 地址
+        _setTokenURI(tokenId, metadataURI); // 使用 `metadataURI` 避免和 `tokenURI` 方法冲突
 
         nextTokenId++;
+    }
+
+    // 仅继承 ERC721URIStorage 进行 override
+    function tokenURI(uint256 tokenId) public view override(ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
