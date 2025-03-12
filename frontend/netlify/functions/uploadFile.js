7ce08d5746a53e413112b573a98d449fe58b086e
bble <bblewong@hotmail.com>
2025-03-12 22:48:15 +0800

    Update uploadFile.js

--- frontend/netlify/functions/uploadFile.js
+++ frontend/netlify/functions/uploadFile.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch');
-
+console.log(fetch);  // 测试是否成功导入
 export async function handler(event) {
   const formData = new FormData();
   formData.append("file", event.body.file);
